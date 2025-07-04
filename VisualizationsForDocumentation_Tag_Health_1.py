ed8895b464b69b1d18fa7757ff2dd4dee78e86cd
23gs1994 <gourishankar@actionfi.com>
2025-07-04 14:52:08 +0530

    finalDashboard2

--- VisualizationsForDocumentation_Tag_Health_1.py
+++ VisualizationsForDocumentation_Tag_Health_1.py
@@ -1163,7 +1163,7 @@ def get_hour_range(hour):
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 8050))  # Use PORT from Render if available
-    # app.run(host="0.0.0.0", port=port, debug=True)
-    app.run(debug=True)  # For local testing
+    port = int(os.environ.get("PORT", 8050))  # Use PORT from Render if available
+    app.run(host="0.0.0.0", port=port, debug=True)
+    # app.run(debug=True)  # For local testing
 
